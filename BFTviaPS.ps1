e6c675a59a34e381454b3b50fd28e6a0150019d7
Ulrik Boesen <125751106+UBoesen@users.noreply.github.com>
2024-10-13 20:46:18 +0200

    Create BFTviaPS.ps1

---
+++ BFTviaPS.ps1
@@ -0,0 +1,51 @@
+# Prompt user for confirmation
+$confirmation = Read-Host "Is this a gaming PC? (yes/no)"
+
+# If user confirms, install the gaming platforms using winget
+if ($confirmation -eq 'yes') {
+    Write-Host "Installing applications silently..."
+
+    # List of specific winget commands to install gaming platforms
+    $wingetCommands = @(
+        { winget install --id=Valve.Steam -e --silent },
+        { winget install --id=EpicGames.EpicGamesLauncher -e --silent },
+        { winget install --id=ElectronicArts.EADesktop -e --silent },
+        { winget install --id=Ubisoft.Connect -e --silent },
+        { winget install --id=REALiX.HWiNFO -e --silent --location "C:\Program FilesBattle.net\" }
+        { winget install --id=7zip.7zipe -e --silent }
+        { winget install --id=REALiX.HWiNFO -e --silent }
+        { winget install --id=Mozilla.Firefox -e --silent }
+        { winget install --id=Notepad++.Notepad++ -e --silent }
+        { winget install --id=SteelSeries.GG -e --silent }
+        { winget install --id=Ghisler.TotalCommander -e --silent }
+        { winget install --id=VideoLAN.VLC -e --silent }
+        { winget install --id=Ookla.Speedtest.Desktop -e --silent }
+        { winget install --id=AgileBits.1Password -e --silent }
+        { winget install --id=Corsair.iCUE.5 -e --silent }
+        { winget install --id=Nvidia.GeForceExperience -e --silent }
+        { winget install --id=Telegram.TelegramDesktop -e --silent }
+        { winget install --id=Discord.Discord -e --silent }
+    )
+
+    # Execute each winget command
+    foreach ($command in $wingetCommands) {
+        Write-Host "Executing $command..."
+        & $command
+        Write-Host "Command executed silently!"
+    }
+
+ # Download Rockstar Games launcher
+    $rockstarUrl = "https://gamedownloads.rockstargames.com/public/installer/Rockstar-Games-Launcher.exe"
+    $downloadPath = "%UserProfile%\Downloads\Rockstar-Games-Launcher.exe" # Make sure the directory exists or specify another path
+    
+    Invoke-WebRequest -Uri $rockstarUrl -OutFile $downloadPath
+    
+    # Run Battle.net installer
+    # Note: Battle.net might not support silent installation. User interaction might be required.
+    Start-Process -FilePath $downloadPath
+
+    Write-Host "All gaming platforms installed!"
+}
+else {
+    Write-Host "Okay, no gaming platforms will be installed."
+}
